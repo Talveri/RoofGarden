3bfbd19af7d6494b72cd0e83940bc0b1196dad81
Talveri <talveri124@gmail.com>
2026-03-13 21:19:18 +0100

    Tilemap, Movement and Environment Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    float x_direction;
-    float y_direction;
     Vector2 xy_direction;
     public float speed = 5f;
     Animator anim;
     public KeyCode useKey = KeyCode.E;
-    public bool useItem = false;
 
     private SpriteRenderer sr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -24,36 +23,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        Movement();
-        // Use
-
-        if (Input.GetKeyDown(useKey))
-        {
-            anim.SetTrigger("use");
-            
-        }
     }
 
-    void FixedUpdate()
+    public void Move(InputAction.CallbackContext context)
     {
-        xy_direction = new Vector2(x_direction, y_direction);
+        xy_direction = context.ReadValue<Vector2>();
+
         xy_direction.Normalize();
         rb.linearVelocity = xy_direction * speed;
-
+        
         // Animation
         anim.SetBool("isRunning", rb.linearVelocity.magnitude != 0);
+
+        if (xy_direction.x < 0)
+            sr.flipX = true;
+        else if (xy_direction.x > 0)
+            sr.flipX = false;
+
+
+        
     }
 
-    void Movement()
+    public void Use()
     {
-        // INPUT
-        x_direction = Input.GetAxisRaw("Horizontal");
-        y_direction = Input.GetAxisRaw("Vertical");
-
-        //FlipSprite
-        if (x_direction < 0)
-            sr.flipX = true;
-        else if (x_direction > 0)
-            sr.flipX = false;
+        anim.SetTrigger("use");
     }
 }
